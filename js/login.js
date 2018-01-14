55149f0cb9cc4a38b2ed5a5d0756fb68dd66de9e
marielacp <marielacp@outlook.com.pe>
2018-01-14 17:47:16 -0500

    añadiendo app warmiTrip

---
+++ js/login.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+  $('#next').hide();
+  /* logueando js con cuenta de google. */
+  var provider = new firebase.auth.GoogleAuthProvider();
+  $('#login').click(function() {
+    // Aparece la ventana pop-up que permite validar
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      saveData(result.user);
+      window.localStorage.setItem('storageUID', result.user.uid);
+      // Guardando el UID en el localstorage
+      var UID = window.localStorage.getItem('storageUID');
+      console.log(UID);
+      // Creando el código de usuario en la rama de posts 
+      firebase.database().ref('posts').set({ UID });
+      // Redireccionando al perfil
+      $('#login').hide();
+      $('#next').show();
+    });
+    
+  });
+
+
+
+  function saveData(user) {
+    console.log(user);
+    var userToSave = {
+      uid: user.uid,
+      name: user.displayName,
+      email: user.email,
+      photo: user.photoURL
+    };
+    // probando es el nombre de tu rama
+
+    firebase.database().ref('bd/' + user.uid).set(userToSave); // push añade un registro 
+  }
+});
+
+
